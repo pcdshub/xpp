e2bfeda298f44fdf1507bc629592b675a627c024
Vincent Esposito <espov@slac.stanford.edu>
2023-07-16 11:05:56 -0700

    exp file

---
+++ experiments/c00121.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from ophyd import EpicsSignal
+from ophyd import EpicsSignalRO
+
+
+SET_WAIT = 2
+
+
+class SettleSignal(EpicsSignal):
+    def __init__(self, *args, settle_time=None, **kwargs):
+        self._settle_time = settle_time
+        super().__init__(*args, **kwargs)
+    
+    def set(self, *args, **kwargs):
+        return super().set(*args, settle_time=self._settle_time, **kwargs)
+
+class User():
+    def __init__(self):
+        self.energy_set = SettleSignal('XPP:USER:MCC:EPHOT:SET1', name='energy_set', settle_time=SET_WAIT)
+        self.energy_ref = SettleSignal('XPP:USER:MCC:EPHOT:REF1', name='energy_ref')
